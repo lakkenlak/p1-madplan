5383047f4d6608b7ede17bbe2894298b7264a089
Jack <Jackvwh@hotmail.com>
2022-11-12 10:24:01 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,50 @@
-int main(void)
-{
-    return 1;
+int main(void){
 
-    s
-    defaultsadw
-    _USE_DECLSPECS_FOR_SALds
-    af
-    asf
 
-    nice code! :)
+    //read recipe file
+    //read fridge file
+    //read user config file
 
-    
+    while (1)
+    {
+        //ask user for input
+            //print fridge(1)
+            //add item to fridge(2)
+            //remove item from fridge(3)
+            //get recipe from fridge(4)
+            //search for a recipe(5)
+            //write "exit" to exit program
+        //interperet user input and call function based on input
+    }
+    add_fridge_content(){}
+
+
+    get_recipe_from_list(fridge_list[], recipe_list[],);
+    return 0;
 }
+
+
+read_fridge_from_file(){}
+read_recipes_from_file(){}
+read_user_config_from_file(){}
+
+interperet_user_input(){
+    //read user input
+    //decide what action to take
+}
+
+print_fridge_content(){
+    //format input array to string
+    //output string
+}
+add_fridge_content(){}
+remove_fridge_content(){}
+
+get_recipe_from_list(){
+    //read fridge list
+    //search recipe with fridge list items
+    //print recipees 
+
+}
+    
+search_for_recipe(){}
