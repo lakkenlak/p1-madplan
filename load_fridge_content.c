c52cbc87a3846db618292a3290417272db9d85f5
Jack <Jackvwh@hotmail.com>
2022-11-13 12:28:14 +0100

    copied load algoritm to both load fn

--- load_fridge_content.c
+++ load_fridge_content.c
@@ -1,25 +1,4 @@
 
-// copy a file list to an array
-// NEEDS modification
-
-#define "filename" // list filename
-
-void copy_fridge_content (){
-    char filename[20];
-    FILE *myFile;
-    int array[N];
-    int k;
-
-    myFile = fopen(filename); // input fridgelist name 
-
-    /* indlæs fil ind i array */
-    for (k = 0; k < N; k++){
-        fscanf(myFile, "%s", &array[k]); // scan in fridge content
-    }
-    fclose(myFile);
-
-
-}
         // FOUND THESE READ/COPY STRUCT ARRAY FILE ALGORITM IN IMPR-C BOOK
         // COULD MODIFY TO READ STRUCT
 /*
