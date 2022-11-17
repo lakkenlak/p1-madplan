f98f41a8f89f6b965eefa6511df136f62bd69e42
FarhanAtHome <116263572+FarhanAtHome@users.noreply.github.com>
2022-11-17 10:19:58 +0100

    Created dummy fridge.txt file & written read_fridge_from_file() function to main.c

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "menu.c"
+#include "Food.c"
 
 //define recipe struct
 
 //define fridge item struct
+
+Food *read_fridge_from_file();
 
 int main(void) {
 
@@ -15,6 +19,51 @@ int main(void) {
     print_main_menu();
 
     return 0;
+}
+
+/*
+*   Returns a Food array that contains the contents of fridge.txt as Food structs
+*/
+Food* read_fridge_from_file(){
+    int i = 0, j = 0, count = 1;
+    char line[150];
+    char *raw_values;
+    Food *food_array;
+    Food new;
+    FILE * fridge_content_txt;
+    fridge_content_txt = fopen("fridge.txt", "r");
+    if(fridge_content_txt == NULL){
+        fridge_content_txt = fopen("fridge.txt", "a+");
+    }
+    while(fgets(line, 150, fridge_content_txt) != NULL){
+        count++;
+    }
+    rewind(fridge_content_txt);
+    food_array = (Food *) malloc(sizeof(Food) * (count+50));
+
+    while(fgets(line, 150, fridge_content_txt) != NULL){
+        char saved_values[5][150] = {"", "", "", "", ""};
+        //printf("%s \n", line); temp line for test
+
+        raw_values = strtok(line, ",");
+        strcat(saved_values[0], raw_values);
+        
+        raw_values = strtok(NULL, ",");
+        strcat(saved_values[1], raw_values);
+        
+        raw_values = strtok(NULL, ",");
+        strcat(saved_values[2], raw_values);
+        
+        raw_values = strtok(NULL, ",");
+        strcat(saved_values[3], raw_values);
+        
+        raw_values = strtok(NULL, ",");
+        strcat(saved_values[4], raw_values);
+        
+        food_array[i] = food_constructor(saved_values[0], saved_values[1], strtod(saved_values[2], NULL), atoi(saved_values[3]), atoi(saved_values[4]));
+        i++;
+    }
+    return food_array;
 }
 
 /*
